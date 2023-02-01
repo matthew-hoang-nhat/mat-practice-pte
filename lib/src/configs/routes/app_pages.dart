a1f8ebc70018c1e0f9aaf45a33060d814bc51f1d
levi <levihoang@github.com>
2023-02-01 17:32:25 +0700

    fix: lack file

--- lib/src/configs/routes/app_pages.dart
+++ lib/src/configs/routes/app_pages.dart
@@ -1,15 +1,6 @@
 import 'package:go_router/go_router.dart';
-import 'package:mat_practice_pte/src/features/auth/ui/login_screen.dart';
-
-import 'app_paths.dart';
 
 class AppPages {
   static const List<String> needAuthenticatedPages = [];
-  static final pages = <GoRoute>[
-    GoRoute(
-      path: '/',
-      name: AppPaths.login,
-      builder: (context, state) => const LoginScreen(),
-    ),
-  ];
+  static final pages = <GoRoute>[];
 }
