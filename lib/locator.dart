9453d4136039577b48240dacad2142b51e92bd22
levi <levihoang@github.com>
2023-02-02 17:57:04 +0700

    feature: FirebaseOptions.dart

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,22 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mat_practice_pte/src/configs/locate/ma_locate.dart';
 import 'package:mat_practice_pte/src/utils/global_variables.dart';
-
+import 'firebase_options.dart';
 import 'src/configs/routes/app_router.dart';
 
 Future<void> locator() async {
   WidgetsFlutterBinding.ensureInitialized();
-  FirebaseOptions? firebaseOptions;
-  if (kIsWeb) {
-    firebaseOptions = const FirebaseOptions(
-        apiKey: 'AIzaSyBmE5ecsJAdikT_-mk1idpeqoF9R_laGzw',
-        appId: "1:611444805102:web:0ddc5bedb11fc62f2ad2ff",
-        messagingSenderId: '611444805102',
-        projectId: '"mat-practice-pte"');
-  }
-  await Firebase.initializeApp(options: firebaseOptions);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setupDependenciesGraph();
 }
 
